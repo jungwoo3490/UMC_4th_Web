89c1976692c8bc9ee9237a5e24fde5046cd4c94f
jungwoo3490 <jungwoo3490@naver.com>
2023-04-07 22:01:39 +0900

    한국어 페이지 - 최상단 컨테이너 추가 완료

--- 3rd_week_study/src/pages/KoreanPage.jsx
+++ 3rd_week_study/src/pages/KoreanPage.jsx
@@ -1,13 +1,9 @@
-import HeaderBar from "../components/HeaderBar";
-import HeaderBox from "../components/HeaderBox";
-import HeaderScreen from "../components/HeaderScreen";
+import HeaderContainer from "../containers/HeaderContainer";
 
 const KoreanPage = () => {
     return (
         <div className="KoreanPage">
-            <HeaderBar />
-            <HeaderBox />
-            <HeaderScreen />
+            <HeaderContainer />
         </div>
     );
 };
