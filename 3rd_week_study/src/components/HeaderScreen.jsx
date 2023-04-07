18b4a797345653c9d91ca04ca9396248d8df4b21
jungwoo3490 <jungwoo3490@naver.com>
2023-04-07 19:42:52 +0900

    JSX 문법에 맞게 태그 속성 수정 완료

--- 3rd_week_study/src/components/HeaderScreen.jsx
+++ 3rd_week_study/src/components/HeaderScreen.jsx
@@ -2,7 +2,7 @@ const HeaderScreen = () => {
     return (
         <div className="HeaderScreen">
             <img id="back1" src="https://assets.nflxext.com/ffe/siteui/vlv3/2e884ce2-da1c-4501-ab9a-10c534d30975/fcdbcd90-f133-4081-ac82-34918f18aabf/KR-ko-20230327-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt=""/>
-            <div id="backcon" style="background-image:linear-gradient(to top, rgba(0, 0, 0, 0.8) 0, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%);" data-uia="nmhp-card-hero+gradient"></div>
+            <div id="backcon" style={{ backgroundImage: 'linear-gradient(to top, rgba(0, 0, 0, 0.8) 0, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%)' }} data-uia="nmhp-card-hero+gradient"></div>
         </div>
     )
 };
