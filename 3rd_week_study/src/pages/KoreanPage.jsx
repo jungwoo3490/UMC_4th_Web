80730f5d95b7f65f1d8f70502fc3c1ccd459f11f
jungwoo3490 <jungwoo3490@naver.com>
2023-04-07 19:45:01 +0900

    한국어 페이지 - 첫 번째 컴포넌트 요소들 자식 컴포넌트로 추가 완료

--- 3rd_week_study/src/pages/KoreanPage.jsx
+++ 3rd_week_study/src/pages/KoreanPage.jsx
@@ -1,7 +1,13 @@
+import HeaderBar from "../components/HeaderBar";
+import HeaderBox from "../components/HeaderBox";
+import HeaderScreen from "../components/HeaderScreen";
+
 const KoreanPage = () => {
     return (
         <div className="KoreanPage">
             <HeaderBar />
+            <HeaderBox />
+            <HeaderScreen />
         </div>
     );
 };
