ecf040b415788f2d77725ff1c1582bb7d85a7918
jungwoo3490 <jungwoo3490@naver.com>
2023-04-07 22:00:18 +0900

    첫번째 컨테이너

---
+++ 3rd_week_study/src/containers/HeaderContainer.jsx
@@ -0,0 +1,16 @@
+import HeaderBar from "../components/HeaderBar";
+import HeaderBox from "../components/HeaderBox";
+import HeaderScreen from "../components/HeaderScreen";
+
+const HeaderContainer = () => {
+    return (
+        <div>
+            <HeaderBar />
+            <HeaderBox />
+            <HeaderScreen />
+        </div>
+    );
+
+}
+
+export default HeaderContainer;
