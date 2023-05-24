a3e7027ef7dd6b44e028717bfe05f95bcbd74340
jungwoo3490 <jungwoo3490@naver.com>
2023-05-25 02:22:54 +0900

    10주차 미션 - Material-UI 실습

--- 10th_week_study/src/App.js
+++ 10th_week_study/src/App.js
@@ -1,6 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
 function Header() {
   return (
     <header>
@@ -19,26 +32,57 @@ function Nav() {
     </nav>
   )
 }
+function Article() {
+  
+  const [open, setOpen] = useState(false);
 
+  return (
+    <article>
+      <h2>Welcome</h2>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      <br />
+      <ButtonGroup>
+        <Button variant="outlined" onClick={() => {
+          setOpen(true);
+        }}>
+          Create
+        </Button>
+        <Button variant="outlined">Update</Button>
+      </ButtonGroup>
+      <Button variant="outlined">Delete</Button>
+      <Dialog open={open}>
+        <DialogTitle>Create</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Hello Create!!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+        <Button variant="outlined">Create</Button>
+        <Button variant="outlined" onClick={() => {
+          setOpen(false);
+        }}>
+          Cancel
+        </Button>
+        </DialogActions>
+      </Dialog>
+    </article> 
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fixed>
+      <Header />
+      <Grid container>
+        <Grid item xs="2">
+          <Nav />
+        </Grid>
+        <Grid item xs="10">
+          <Article />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
